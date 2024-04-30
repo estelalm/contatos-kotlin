b30890f784c70bbe6dde38148b8b92ed7594a584
Estela Alves. <123739850+estelalm@users.noreply.github.com>
2024-04-30 12:22:07 -0300

    banco de dados

--- app/src/main/java/br/senai/sp/jandira/contatos/dao/ContatosDb.kt
+++ app/src/main/java/br/senai/sp/jandira/contatos/dao/ContatosDb.kt
@@ -1,5 +1,6 @@
 package br.senai.sp.jandira.contatos.dao
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -13,7 +14,25 @@ abstract class ContatosDb : RoomDatabase (){
 
     abstract fun contatoDao(): ContatoDao
 
-    //mesma coisa dos atatic do java
-    companion object
+    //mesma coisa do static do java
+    companion object{
+        //lateinit: espara algo acontecer para depois funcionar (  como o await  )
+        private lateinit var instancia: ContatosDb
+    }
+
+    fun getBancoDeDados(context: Context): ContatosDb{
+        instancia = Room
+            .databaseBuilder(
+                context,  // contexto passado na chamada da função
+                ContatosDb::class.java, // instancia o contatosdb
+                "db_contatos" // nome do banco
+            )
+            .allowMainThreadQueries()  // permitir chamar o banco de dados na mesma thread que controla a interface gráfica
+            .fallbackToDestructiveMigration()  // ignora a versão do banco, destrói ao invés de fazer a migração
+            .build()
+
+        return instancia
+    }
+
 
 }
